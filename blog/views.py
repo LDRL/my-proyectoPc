fd9719b979d2d8a64674681e596cf92e732f9511
ldrl <drdanielreyes5@gmail.com>
2016-11-19 01:42:55 -0600

    Creacion de un listado Categoria

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Categoria
 
 def index(request):
     return render(request, 'blog/index.html', {})
+
+def listado(request):
+    return render(request, 'blog/categoria/listado.html',{})
+
+#def index(request):
+#    return render(request, 'blog/index.html'.{})
 # Create your views here.
